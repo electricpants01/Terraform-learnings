24c3fd602f2ff96516a3b43229a0c185fd3b74b0
Christian Torrico <christian.torrico-contractor@procore.com>
2024-09-24 20:25:03 -0400

    added imports.tf

---
+++ imports.tf
@@ -0,0 +1,19 @@
+import {
+  to = aws_s3_bucket.awesome_bucket
+  id = "first-awesome-bucket"
+}
+
+import {
+  to = aws_s3_bucket_versioning.my_bucket_versioning
+  id = "first-awesome-bucket"
+}
+
+import {
+  to = aws_s3_bucket_policy.my_bucket_policy
+  id = "first-awesome-bucket"
+}
+
+import {
+  to = aws_s3_bucket_public_access_block.my_bucket_public_access
+  id = "first-awesome-bucket"
+}
